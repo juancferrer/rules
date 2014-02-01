64ce2fadf39dfbdebdc80df470fabe23e90a06a4
Juan Carlos Ferrer <juan.carlos@micronixsolutions.com>
2014-02-01 16:30:10 -0600

    cleanup

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 #include "Rules.h"
 using namespace std;
 
+// Sum of the two working numbers >= to 1000
 ArithmeticRule<function<int(int,int)>, function<bool(int,int)>> 
-    ruleA = ArithmeticRule<function<int(int,int)>,function<bool(int,int)>>("RuleA", plus<int>(), 1000, greater_equal<int>());
+    ruleA = ArithmeticRule<function<int(int,int)>,function<bool(int,int)>>(
+            "RuleA", plus<int>(), 1000, greater_equal<int>());
+
+// New number-500 >= last number
 BooleanRule<function<bool(int,int)>> 
-    ruleB = BooleanRule<function<bool(int,int)>>("RuleB", greater<int>(), -500);
+    ruleB = BooleanRule<function<bool(int,int)>>("RuleB", greater_equal<int>(), -500);
+
+// Average of all the numbers processed is >= 500
 AvgRule<function<bool(int,int)>> 
     ruleC = AvgRule<function<bool(int,int)>>("RuleC", 500, greater_equal<int>());
 
@@ -19,7 +25,7 @@ int main(int argc, char *argv[]){
     }
     cout << ruleA << '\n' << ruleB << '\n' << ruleC << '\n' ;
     if(ruleA && ruleB && ruleC){
-        cout << "ALL rules passed." << '\n';
+        cout << "ALL rules passed" << '\n';
     }
     return 0;
 }
